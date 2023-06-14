5d79cb88dd13c42be8e4b19c6542d975c2da5872
chisom julius <chisomjulius@chisoms-MacBook-Pro.local>
2023-06-14 22:52:23 +0100

    debugging .....

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -48,8 +48,8 @@ const ProjectCard = ({
           </div>
         </div>
         <div className='mt-5'>
-          <h3 className='text-white font-bold text-[20px]'>{name}</h3>
-          <p className='mt-2 text-secondary text-[10px]'>{description.slice(0,20)}</p>
+          <h3 className='text-white font-bold text-[15px] sm:text-[24px]'>{name}</h3>
+          <p className='mt-2 text-secondary text-[10px] sm:text-[15px]'>{description}</p>
         </div>
 
         <div className='flex flex-wrap gap-2'>
